b9e41d927a7105488e2ec8c840cb294422a109b4
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2026-03-02 11:37:04 -0300

    feat: primeiro commit

---
+++ dist/pessoa.d.ts
@@ -0,0 +1,8 @@
+declare class Pessoa {
+    nome: string;
+    idade: number;
+    constructor(nome: string, idade: number);
+    saudacao(): string;
+}
+export { Pessoa };
+//# sourceMappingURL=pessoa.d.ts.map
