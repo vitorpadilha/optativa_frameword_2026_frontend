cc19128174ba989d5baf664f5cd0f4758a94fba5
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2026-03-06 18:36:30 -0300

    Aula 1

--- src/index2.ts
+++ src/index2.ts
@@ -1,10 +1,29 @@
-import { Pessoa } from "./pessoa.js";
+
 
 document.addEventListener("DOMContentLoaded", () => {
-    let p: Pessoa = new Pessoa("João", 30);
-    let divApp: HTMLDivElement | null = document.getElementById("app") as HTMLDivElement | null;
 
-    divApp!.innerHTML += `<p>${p.saudacao()}</p>`;
-    
+    let campoNome: HTMLInputElement | null = document.getElementById("nome") as HTMLInputElement | null;
+    let campoIdade: HTMLInputElement | null = document.getElementById("idade") as HTMLInputElement | null;
+    let btnAdicionar: HTMLButtonElement | null = document.getElementById("btnAdicionar") as HTMLButtonElement | null;
+
+    btnAdicionar?.addEventListener("click", (event) => {
+        event.preventDefault(); // Evita o envio do formulário
+    fetch("http://localhost:3000/pessoas", {
+        method: "POST",
+        headers: {  
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: campoNome?.value,
+            idade: parseInt(campoIdade?.value || "0")
+        })
+    })    .then(response => response.json())
+    .then(data => {
+        console.log("Cadastrado com Sucesso:", data);
+    })
+    .catch(error => {
+        console.error("Erro ao enviar dados:", error);
+    });
+});
 
 });
